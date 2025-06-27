0a0369f29c3d64748dbff2198ed54529c27b279e
Ardana Yatra <imadeardanayatra@gmail.com>
2025-06-28 04:24:31 +0800

    refund otomatis

---
+++ database/migrations/2025_06_27_202414_add_order_id_to_tbl_pembayaran_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('tbl_pembayaran', function (Blueprint $table) {
+            $table->string('order_id')->unique()->after('id_pembayaran');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('tbl_pembayaran', function (Blueprint $table) {
+            $table->dropColumn('order_id');
+        });
+    }
+};
