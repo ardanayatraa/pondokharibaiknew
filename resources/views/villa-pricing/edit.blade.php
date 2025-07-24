2ea06450347474a33771f5d98e8d11767d0be1f4
Ardana Yatra <imadeardanayatra@gmail.com>
2025-07-25 03:39:37 +0800

    lala

--- resources/views/villa-pricing/edit.blade.php
+++ resources/views/villa-pricing/edit.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div class="w-full mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <div class="flex justify-between items-center mb-6">
