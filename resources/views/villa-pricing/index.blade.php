cfde87d9a3f68717f28d30fc1c8c3cef3f0342ab
Ardana Yatra <imadeardanayatra@gmail.com>
2025-07-24 16:09:22 +0800

    lala

--- resources/views/villa-pricing/index.blade.php
+++ resources/views/villa-pricing/index.blade.php
@@ -5,7 +5,7 @@
             <div class="bg-white shadow-lg rounded-lg p-6">
                 <div class="flex justify-between items-center mb-4">
                     <h1 class="text-sm font-semibold text-gray-800">Villa Pricing</h1>
-                    <a href="{{ route('harga-villa.create') }}">
+                    <a href="{{ route('villa-pricing.create') }}">
                         <x-button>Tambah Villa Pricing</x-button>
                     </a>
                 </div>
