603fe088b172b36f4bbfa238b3e7e37bf2f0ac3f
Ardana Yatra <imadeardanayatra@gmail.com>
2025-07-25 11:57:50 +0800

    lala

--- resources/views/reservasi/index.blade.php
+++ resources/views/reservasi/index.blade.php
@@ -1,4 +1,3 @@
-{{-- resources/views/reservasi/index.blade.php --}}
 <x-app-layout>
     <div class="py-2">
         <div class="w-full mx-auto sm:px-6 lg:px-8">
@@ -6,6 +5,14 @@
                 <div class="flex justify-between items-center mb-4">
                     <h1 class="text-sm font-semibold text-gray-800">Reservasi</h1>
                 </div>
+
+                {{-- Tampilkan session error --}}
+                @if (session('error'))
+                    <div class="mb-4 px-4 py-2 bg-red-100 border border-red-400 text-red-700 text-sm rounded">
+                        {{ session('error') }}
+                    </div>
+                @endif
+
                 @livewire('table.reservasi-table')
             </div>
         </div>
