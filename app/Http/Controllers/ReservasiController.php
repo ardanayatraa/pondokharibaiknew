817a08e559fb7df06886b3bc3f2f9f088625283c
Ardana Yatra <imadeardanayatra@gmail.com>
2025-07-25 11:52:50 +0800

    lala

--- app/Http/Controllers/ReservasiController.php
+++ app/Http/Controllers/ReservasiController.php
@@ -120,6 +120,7 @@ class ReservasiController extends Controller
 
     public function checkin(Reservasi $reservasi)
     {
+        dd($reservasi);
         // Hanya reservasi dengan status confirmed yang bisa di-checkin
       if (!in_array($reservasi->status, ['confirmed', 'resheduled'])) {
     return back()->with('error', 'Hanya reservasi dengan status confirmed atau resheduled yang bisa di-checkin.');
